00d53c2c5611086056e2b6f4afe21fe1ed89758b
Bogatyrev-Islam <p79487548@gmail.com>
2026-03-07 13:09:37 +0300

    Добавил конвертер валют

--- lesson-5/src/App.vue
+++ lesson-5/src/App.vue
@@ -1,49 +1,89 @@
-<script >
-  import Input from './components/Input.vue';
-  import Selector from './components/Selector.vue';
+<script>
+import Input from './components/Input.vue';
+import Selector from './components/Selector.vue';
+import CryptoConvert from 'crypto-convert';
 
-  export default{
-    components: {
-      Input,
-      Selector
+const convert = new CryptoConvert();
+
+export default {
+  components: {
+    Input,
+    Selector
+  },
+  data() {
+    return {
+      amount: 0,
+      cryptoFirst: "",
+      cryptoSecond: "",
+      error: '',
+      result: 0
+    }
+  },
+  methods: {
+    changeAmount(val) {
+      this.amount = val
     },
-    data(){
-      return{
-        amount: 0,
-        cryptoFirst: "",
-        cryptoSecond: ""
+    selectCryptoFirst(val) {
+      this.cryptoFirst = val
+    },
+    selectCryptoSecond(val) {
+      this.cryptoSecond = val
+    },
+    async convert() {
+      if (this.amount <= 0) {
+        this.error = 'Введите число больше 0'
+        this.result = 0
+        return;
+      } else if (this.cryptoFirst == this.cryptoSecond) {
+        this.error = "Выберите другую валюту"
+        this.result = 0
+        return
+      } else if (this.cryptoFirst == "" || this.cryptoSecond == "") {
+        this.error = "Выберите валюту"
+        this.result = 0
+        return
       }
-    },
-    methods: {
-      changeAmount(val){
-        this.amount = val
-      },
-      selectCryptoFirst(val){
-        this.cryptoFirst = val
-      },
-      selectCryptoSecond(val){
-        this.cryptoSecond = val
+      this.error = ''
+      await convert.ready()
+
+      if (this.cryptoFirst == "BTC" && this.cryptoSecond == 'ETH') {
+        this.result = convert.BTC.ETH(this.amount)
+      } else if (this.cryptoFirst == "BTC" && this.cryptoSecond == 'USDT') {
+        this.result = convert.BTC.USDT(this.amount)
+      } else if (this.cryptoFirst == "ETH" && this.cryptoSecond == 'BTC') {
+        this.result = convert.ETH.BTC(this.amount)
+      } else if (this.cryptoFirst == "ETH" && this.cryptoSecond == 'USDT') {
+        this.result = convert.ETH.USDT(this.amount)
+      } else if (this.cryptoFirst == "USDT" && this.cryptoSecond == 'BTC') {
+        this.result = convert.USDT.BTC(this.amount)
+      } else if (this.cryptoFirst == "USDT" && this.cryptoSecond == 'ETH') {
+        this.result = convert.USDT.ETH(this.amount)
       }
     }
   }
+}
 </script>
 
 <template>
   <h1>Crypto</h1>
-  <Input :changeAmount="changeAmount"/>
-  {{ cryptoFirst }}
-  {{ cryptoSecond }}
+  <Input :changeAmount="changeAmount" :convert="convert" />
+  <p v-if="error != ''" class="error-text">{{ error }}</p>
+  <p v-if="result != 0" class="result-text">{{ Math.floor(result) }}</p>
   <div class="selectors">
-    <Selector :setCrypto = "selectCryptoFirst"/>
-    <Selector :setCrypto = "selectCryptoSecond"/>
+    <Selector :setCrypto="selectCryptoFirst" />
+    <Selector :setCrypto="selectCryptoSecond" />
   </div>
 </template>
 
 <style scoped>
-  .selectors{
-    display: flex;
-    justify-content: space-around;
-    width: 700px;
-    margin: 0 auto;
-  }
+.selectors {
+  display: flex;
+  justify-content: space-around;
+  width: 700px;
+  margin: 0 auto;
+}
+.result-text{
+  font-family: 'George', cursive;
+  font-size: 2em;
+}
 </style>
--- lesson-5/src/components/Input.vue
+++ lesson-5/src/components/Input.vue
@@ -1,46 +1,62 @@
 <script>
-    export default{
-      props:{
-        changeAmount:{
-          type: Function,
-          required: true
-        }
-      }
+export default {
+  props: {
+    changeAmount: {
+      type: Function,
+      required: true
+    },
+    convert: {
+      type: Function,
+      required: true
     }
+  }
+}
 </script>
 
 <template>
   <input min="0" type="number" @input="changeAmount($event.target.value)" placeholder="Введите сумму"> <br>
-  <button>Конвертировать</button>
+  <button @click="convert()">Конвертировать</button>
+  <button>В избранное</button>
 </template>
 
 <style scoped>
-    input{
-    width: 500px;
-    position: relative;
-    top: -50px;
-    font-size: 17px;
-    text-align: center;
-    border-radius: 3px;
-    border: none;
-    padding: 15px;
-    background-color: #fafafa;
-    color: #333;
-    outline: none;
-    transition: transform 700ms ease;
-  }
-  input:focus{
-    transform: scale(1.1);
-  }
-  button{
-    position: relative;
-    top: -20px;
-    padding: 15px 20px;
-    color: #fff;
-    text-transform: uppercase;
-    cursor: pointer;
-    background: #1a032d;
-    border: none;
-    border-radius: 5px;
-  }
+input {
+  width: 500px;
+  position: relative;
+  top: -50px;
+  font-size: 17px;
+  text-align: center;
+  border-radius: 3px;
+  border: none;
+  padding: 15px;
+  background-color: #fafafa;
+  color: #333;
+  outline: none;
+  transition: transform 700ms ease;
+}
+
+input:focus {
+  transform: scale(1.1);
+}
+
+button {
+  position: relative;
+  top: -20px;
+  padding: 15px 20px;
+  color: #fff;
+  text-transform: uppercase;
+  cursor: pointer;
+  background: #1a032d;
+  border: none;
+  border-radius: 5px;
+  margin: 15px;
+  transition: transform 300ms ease;
+}
+button:hover{
+  transform: scale(1.08);
+  opacity: 90%;
+}
+button:active{
+  transform: scale(0.99);
+}
 </style>
